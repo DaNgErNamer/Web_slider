4ed738065aff17b00de119734b8e74cc5ed90d3e
Kirill Manaenkov <kirill5785945@gmail.com>
2021-04-06 23:02:22 +0300

    Upload 2 laba

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <Slider />
+</template>
+
+<script>
+  import Slider from "./components/Slider.vue";
+
+  export default {
+    name: 'App',
+    components: {
+      Slider
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    background-color: #1e1e1e;
+  }
+    .div
+    {
+        background-color: #1e1e1e;
+    }
+body {
+    margin: 0;
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+    text-align: left;
+    background-color: #1e1e1e !important;
+}
+.btn-success.disabled, .btn-success:disabled {
+    color: #fff !important;
+    height: 100px;
+    width: 300px;
+    background-color: #404040 !important;
+    border-color: #404040 !important;
+    border-radius: 25px;
+    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+}
+
+.btn-success {
+    color: #2c3e50 !important;
+    height: 100px;
+    width: 300px;
+    background-color: #ffb9b9 !important;
+    border-color: #404040 !important;
+    border-radius: 25px  !important;
+    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+}
+
+.btn-info.disabled, .btn-info:disabled {
+    color: #fff !important;
+    background-color: #ff4747 !important;
+    border-color: #17a2b8  !important;
+    border-radius: 25px  !important;
+    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+}
+.btn-info {
+    color: #2c3e50 !important;
+    background-color: #ff4747 !important;
+    border-color: #17a2b8  !important;
+    border-radius: 25px  !important;
+    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="picture">
+        <img class="picture" :src="Paths[count-1]">
+    </div>
+    <controlPanel>
+        <button v-if="count===1" class=" btn btn-success mt-5 mr-2 disabled">Previous Slide
+        </button>
+        <button v-if="count>1" class=" btn btn-success mt-5 mr-2" v-on:click="count>1?count--:count=1">Previous Slide
+        </button>
+        <label class="btn btn-info disabled mt-5 mr-2">Slide {{count}}</label>
+        <button v-if="count<Paths.length" class=" btn btn-success mt-5 mr-2" v-on:click="count++">Next
+            Slide</button>
+        <button v-if="count===Paths.length" class="disabled btn btn-success mt-5 mr-2"
+            v-on:click="count>=Paths.length?count=Paths.length:count++">Next
+            Slide</button>
+    </controlPanel>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 1,
+                Paths: [
+                    require("../assets/pic1.jpg"),
+                    require("../assets/pic2.jpg"),
+                    require("../assets/pic3.jpg"),
+                    require("../assets/pic4.jpg"),
+                    require("../assets/pic5.jpg"),
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+    .picture {
+        width: 1600px;
+        height: 900px;
+        margin: auto;
+    }
+    .root
+    {
+        background-color: #1e1e1e;
+    }
+    .div
+    {
+        background-color: #1e1e1e;
+    }
+     body {
+    margin: 0;
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+    text-align: left;
+    background-color: #1e1e1e !important ;
+}
+</style>
